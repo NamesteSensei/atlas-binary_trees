f71bf5ccfe73c0be066820cf8f5ff0e8c9bbf461
namestesensei <christopherwright435@gmail.com>
2024-08-17 04:53:38 +0000

    Implement binary_tree_balance function with correct linking of dependent files and verify output

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,11 +9,17 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_height, right_height;
+
 	if (tree == NULL)
 		return (0);
 
-	return (1 + binary_tree_height(tree->left) > binary_tree_height(tree->right) ?
-		binary_tree_height(tree->left) : binary_tree_height(tree->right));
+	/* Calculate the height of left and right subtrees */
+	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+	/* Return the greater height */
+	return (left_height > right_height ? left_height : right_height);
 }
 
 /**
