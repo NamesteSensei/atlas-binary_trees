b03208a22f6bf28967bd243c6e3b46f8c3d4ba44
namestesensei <christopherwright435@gmail.com>
2024-08-17 03:49:01 +0000

    Implement binary_tree_delete function and add corresponding tests

--- binary_trees.h
+++ binary_trees.h
@@ -4,13 +4,22 @@
 #include <stddef.h>
 #include <stdlib.h>
 
-/* Structure and type definitions */
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ *
+ * Description: Node structure for a binary tree
+ */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
@@ -19,6 +28,7 @@ typedef struct binary_tree_s binary_tree_t;
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
 void binary_tree_print(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
