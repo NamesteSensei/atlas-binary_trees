ca4d5279c2d20af90bcee6657637dadf5dfd09b3
namestesensei <christopherwright435@gmail.com>
2024-08-17 03:39:43 +0000

    Implement binary_tree_insert_right function and add corresponding tests

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,39 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - Inserts a node as the right-child of another node
+ *
+ * @parent: Pointer to the node to insert the right-child in
+ * @value: Value to store in the new node
+ *
+ * Return: Pointer to the created node, or NULL on failure or if parent is NULL
+ *
+ * Description: If the parent already has a right-child, the new node
+ * takes its place, and the old right-child becomes the right-child of the
+ * new node.
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	if (parent == NULL)
+		return (NULL);
+
+	/* Create the new node */
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+		return (NULL);
+
+	/* If parent already has a right child, set it as the right child of the new node */
+	if (parent->right != NULL)
+	{
+		new_node->right = parent->right;
+		parent->right->parent = new_node;
+	}
+
+	/* Set the new node as the right child of the parent */
+	parent->right = new_node;
+
+	return (new_node);
+}
+
--- binary_trees.h
+++ binary_trees.h
@@ -4,14 +4,7 @@
 #include <stddef.h>
 #include <stdlib.h>
 
-/**
- * struct binary_tree_s - Binary tree node
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- * Description: Node structure for a binary tree
- */
+/* Structure and type definitions */
 struct binary_tree_s
 {
     int n;
@@ -25,6 +18,7 @@ typedef struct binary_tree_s binary_tree_t;
 /* Function prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
