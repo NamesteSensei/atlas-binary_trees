401651f11a124f12d126e2d14f6e142197a7e4f2
namestesensei <christopherwright435@gmail.com>
2024-08-17 03:54:44 +0000

    Implement binary_tree_is_leaf function and add corresponding tests

--- binary_trees.h
+++ binary_trees.h
@@ -4,16 +4,7 @@
 #include <stddef.h>
 #include <stdlib.h>
 
-/**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- *
- * Description: Node structure for a binary tree
- */
+/* Structure and type definitions */
 struct binary_tree_s
 {
 	int n;
@@ -29,6 +20,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
+int binary_tree_is_leaf(const binary_tree_t *node);
 void binary_tree_print(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
