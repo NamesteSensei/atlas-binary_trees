f466e650ccb050d4126ce2502cc8e43f9cf150d1
namestesensei <christopherwright435@gmail.com>
2024-08-17 05:35:25 +0000

    Implement binary_tree_uncle function and add corresponding tests

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - Finds the uncle of a node
+ * @node: Pointer to the node to find the uncle
+ * Return: Pointer to the uncle node, or NULL if node is NULL, parent is NULL,
+ * or node has no uncle
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
+
+	/* Check if the parent of the node is the left or right child and return the sibling */
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	else if (node->parent->parent->right == node->parent)
+		return (node->parent->parent->left);
+
+	return (NULL);
+}
+
